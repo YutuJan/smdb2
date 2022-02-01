19d5cbdaafa81fad9171fc46b10f359fe17cb5b1
Sirinian Aram Emmanouil <ae.sirinian@gmail.com>
2022-02-01 19:19:26 +0200

    Updated MovieRepositoryTest.

--- smdb2-app/src/test/java/gr/sae/smdb2/repository/MovieRepositoryTest.java
+++ smdb2-app/src/test/java/gr/sae/smdb2/repository/MovieRepositoryTest.java
@@ -4,7 +4,12 @@ import gr.sae.base.AbstractLogComponent;
 import gr.sae.domain.Genre;
 import gr.sae.domain.Movie;
 import gr.sae.repository.MovieRepository;
-import org.junit.jupiter.api.Test;
+import gr.sae.transfer.MoviesAndSeriesPerGenreDto;
+import gr.sae.transfer.MoviesPerGenrePerYearDto;
+import gr.sae.transfer.TopRatedMovieDto;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -13,6 +18,7 @@ import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class MovieRepositoryTest extends AbstractLogComponent {
@@ -41,10 +47,15 @@ class MovieRepositoryTest extends AbstractLogComponent {
     private final String MOVIE_TITLE_001 = "Zorba the Greek";
     private final String MOVIE_TITLE_002 = "Rush Hour";
 
-    @Test
-    void findMovieByTitle() {
-        createBunchOfMovies();
+    List<Movie> movies;
 
+    @BeforeEach
+    void setUp() {
+        createBunchOfMovies();
+    }
+
+    @Test
+    void ensureFindMovieByTitle() {
         // given
         String title = MOVIE_TITLE_002;
 
@@ -57,9 +68,7 @@ class MovieRepositoryTest extends AbstractLogComponent {
     }
 
     @Test
-    void getMovieByTitle() {
-        createBunchOfMovies();
-
+    void ensureGetMovieByTitle() {
         // given
         String title = MOVIE_TITLE_002;
 
@@ -71,8 +80,144 @@ class MovieRepositoryTest extends AbstractLogComponent {
         assertThat(title).isEqualTo(movie.getTitle());
     }
 
+    @Test
+    void ensureFindMoviesByGenresContaining() {
+        // given
+        Movie actionMovie0 = movies.get(0);
+        Movie actionMovie1 = movies.get(2);
+        Movie nonActionMovie = movies.get(1);
+
+        // when
+        List<Movie> actionMovies = movieRepository.findMoviesByGenresContaining(Genre.ACTION);
+
+        // then
+        assertAll("Ensure that the repository returns all action movies and no other movies.",
+                () -> assertTrue(checkIfAListOfMoviesContainAMovie(actionMovies, actionMovie0),
+                        "Movie with title " + actionMovie0.getTitle() + " must be in action movies."),
+                () -> assertTrue(checkIfAListOfMoviesContainAMovie(actionMovies, actionMovie1),
+                        "Movie with title " + actionMovie1.getTitle() + " must be in action movies."),
+                () -> assertFalse(checkIfAListOfMoviesContainAMovie(actionMovies, nonActionMovie),
+                        "Movie with title " + nonActionMovie.getTitle() + " must not be in action movies."),
+                () -> assertEquals(2, actionMovies.size(),
+                        "Total number of action movies must be 2."));
+    }
+
+    @Test
+    void ensureFindMoviesByRatingIsGreaterThanEqual() {
+        // given
+        Movie movieWithRatingGreaterThan7_0 = movies.get(0);
+        Movie movieWithRatingGreaterThan7_1 = movies.get(1);
+        Movie movieWithRatingNotGreaterThan7 = movies.get(2);
+
+        // when
+        List<Movie> moviesWithRatingGreaterThan7 = movieRepository.findMoviesByRatingIsGreaterThanEqual(7.1);
+
+        // then
+        assertAll("Ensure that the repository returns all movies with rating greater or equal than 7.1 " +
+                        "and no other movies.",
+                () -> assertTrue(checkIfAListOfMoviesContainAMovie(
+                        moviesWithRatingGreaterThan7, movieWithRatingGreaterThan7_0),
+                        "Movie with title " + movieWithRatingGreaterThan7_0.getTitle() +
+                                " must be movies with rating greater or equal than 7.1."),
+                () -> assertTrue(checkIfAListOfMoviesContainAMovie(
+                        moviesWithRatingGreaterThan7, movieWithRatingGreaterThan7_1),
+                        "Movie with title " + movieWithRatingGreaterThan7_1.getTitle() +
+                                " must be movies with rating greater or equal than 7.1"),
+                () -> assertFalse(checkIfAListOfMoviesContainAMovie(
+                        moviesWithRatingGreaterThan7, movieWithRatingNotGreaterThan7),
+                        "Movie with title " + movieWithRatingNotGreaterThan7.getTitle() +
+                                " must be movies with rating greater or equal than 7.1"),
+                () -> assertEquals(2, moviesWithRatingGreaterThan7.size(),
+                        "Total number of  movies with rating greater or equal than 7.1 must be 2."));
+    }
+
+    private boolean checkIfAListOfMoviesContainAMovie(List<Movie> movies, Movie movie) {
+        for (Movie m: movies) {
+            if (m.getTitle().equals(movie.getTitle())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @DisplayName("Grouped tests for checking MovieRepository.findMoviesPerGenre() method.")
+    @Nested
+    class EnsureFindMoviesPerGenre {
+        @ParameterizedTest(name = "moviesAndSeriesPerGenre.get({0}).getGenre() == {1}")
+        @CsvSource({"0, ACTION", "1, COMEDY", "2, DRAMA", "3, SCI_FI", "4, SPORTS"})
+        void checkGenreFromFindMoviesPerGenre(int position, String genre) {
+            // when
+            List<MoviesAndSeriesPerGenreDto> moviesAndSeriesPerGenre = movieRepository.findMoviesPerGenre();
+
+            // then
+            assertEquals(moviesAndSeriesPerGenre.get(position).getGenre().toString(), genre);
+        }
+
+        @ParameterizedTest(name = "moviesAndSeriesPerGenre.get({0}).getCount() == {2}")
+        @CsvSource({"0, 2", "1, 1", "2, 1", "3, 1", "4, 1"})
+        void checkCountFromFindMoviesPerGenre(int position, int count) {
+            // when
+            List<MoviesAndSeriesPerGenreDto> moviesAndSeriesPerGenre = movieRepository.findMoviesPerGenre();
+
+            // then
+            assertEquals(moviesAndSeriesPerGenre.get(position).getCount(), count);
+        }
+    }
+
+    @DisplayName("Grouped tests for checking MovieRepository.findMoviesPerGenrePerYear() method.")
+    @Nested
+    class EnsureFindMoviesPerGenrePerYear {
+        @ParameterizedTest(name = "moviesPerGenrePerYear.get({0}).getGenre() == {2}")
+        @CsvSource({"0, ACTION", "1, ACTION", "2, COMEDY", "3, DRAMA", "4, SCI_FI", "5, SPORTS"})
+        void ensureGenreFromFindMoviesPerGenrePerYear(int position, String genre) {
+            // when
+            List<MoviesPerGenrePerYearDto> moviesPerGenrePerYear = movieRepository.findMoviesPerGenrePerYear();
+
+            // then
+            assertEquals(moviesPerGenrePerYear.get(position).getGenre().toString(), genre);
+        }
+
+        @ParameterizedTest(name = "moviesPerGenrePerYear.get({0}).getCount() == {2}")
+        @CsvSource({"0, 1", "1, 1", "2, 1", "3, 1", "4, 1", "5, 1"})
+        void ensureCountFromFindMoviesPerGenrePerYear(int position, int count) {
+            // when
+            List<MoviesPerGenrePerYearDto> moviesPerGenrePerYear = movieRepository.findMoviesPerGenrePerYear();
+
+            // then
+            assertEquals(moviesPerGenrePerYear.get(position).getCount(), count);
+        }
+
+        @ParameterizedTest(name = "moviesPerGenrePerYear.get({0}).getYear() == {2}")
+        @CsvSource({"0, 1999", "1, 2007", "2, 2007", "3, 1964", "4, 1999", "5, 2007"})
+        void ensureYearFromFindMoviesPerGenrePerYear(int position, int year) {
+            // when
+            List<MoviesPerGenrePerYearDto> moviesPerGenrePerYear = movieRepository.findMoviesPerGenrePerYear();
+
+            // then
+            assertEquals(moviesPerGenrePerYear.get(position).getYear(), year);
+        }
+    }
+
+    @Test
+    void ensureFindTopRatedMovie() {
+        // given
+        Movie actualTopRatedMovie = movies.get(0);
+
+        // when
+        TopRatedMovieDto topRatedMovie = movieRepository.findTopRatedMovie();
+
+        // then
+        assertAll("Ensure that the repository returns the top rated movie.",
+                () -> assertEquals(actualTopRatedMovie.getTitle(), topRatedMovie.getTitle(),
+                        "Top rated movie must have title " + topRatedMovie.getTitle()),
+                () -> assertEquals(actualTopRatedMovie.getSummary(), topRatedMovie.getSummary(),
+                        "Top rated movie must have summary" + topRatedMovie.getSummary()),
+                () -> assertEquals(actualTopRatedMovie.getRating(), topRatedMovie.getRating(),
+                        "Top rated movie must have rating" + topRatedMovie.getRating()));
+    }
+
     private void createBunchOfMovies() {
-        List<Movie> movies = List.of(
+        movies = List.of(
                 Movie.builder()
                         .title(MOVIE_TITLE_000)
                         .summary(SUMMARY_000)
